2f544b3c595829fb4550926488b2981aaf8b5a8b
FelippeC <felippejcampos@gmail.com>
2019-09-27 22:21:35 +0200

    Use __init__ to run the app

--- app/__init__.py
+++ app/__init__.py
@@ -53,3 +53,9 @@ def not_found(error):
 @app.route("/")
 def index():
     return "Hello!"
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
