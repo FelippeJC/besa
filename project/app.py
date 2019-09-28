ffdb70970cdfd2e44d4de5604ccf8d3a27ce80c6
FelippeC <felippejcampos@gmail.com>
2019-09-28 16:03:13 +0200

    Adding front-end files

--- project/app.py
+++ project/app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from config import Config, DevelopmentConfig, TestingConfig
 from .database import db
+import folium
 
 ##########################################################################
 # Configurations
@@ -52,7 +53,56 @@ def not_found(error):
 
 @app.route("/")
 def index():
-    return "Hello!"
+    return render_template('index.html')
+
+
+@app.route("/city-initiatives")
+def city_initiatives():
+    return render_template('city_initiatives.html')
+
+
+@app.route("/city-amenities")
+def city_amenities():
+    m = folium.Map(location=[45.5236, -122.6750],
+                   tiles='Stamen Toner',
+                   zoom_start=13,
+                   )
+
+    folium.Circle(radius=100,
+                  location=[45.5244, -122.6699],
+                  popup='The Waterfront',
+                  color='crimson',
+                  fill=True,
+                  ).add_to(m)
+
+    folium.CircleMarker(location=[45.5215, -122.6261],
+                        radius=50,
+                        popup='Laurelhurst Park',
+                        color='#3186cc',
+                        fill=True,
+                        fill_color='#3186cc'
+                        ).add_to(m)
+    legend_html = '''
+                <div style="position: fixed; background-color: rgba(255,255,255,0.85);
+                            bottom: 50px; left: 50px; width: 100px; height: 90px;
+                            border:2px solid grey; z-index:9999; font-size:14px;">
+                    &nbsp; Cool Legend <br>
+                    &nbsp; <i class="fa fa-map-marker fa-2x" style="color:green"></i> &nbsp; East <br>
+                    &nbsp; <i class="fa fa-map-marker fa-2x" style="color:red"></i> &nbsp; West
+                </div>
+                '''
+    m.get_root().html.add_child(folium.Element(legend_html))
+    return render_template('city_amenities.html', folium_map=m._repr_html_())
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/blank")
+def blank():
+    return render_template('blank.html')
 
 
 if __name__ == '__main__':
